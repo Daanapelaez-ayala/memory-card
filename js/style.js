9cda8f88c5485ce9164fad4e28bf20f4c6c45690
Daanapelaez-ayala <126dpelaezayala@frhsd.com>
2025-04-01 14:05:04 -0400

    23.36

--- js/style.js
+++ js/style.js
@@ -1,1 +1,61 @@
 const card = document.querySelectorAll(".card");
+
+let matchedCard = 0
+let cardOne, cardTwo;
+let disableDeck = false;
+
+function flipCard(e){
+    let clickCard = e.target;
+    if(clickCard !== cardOne && !disableDeck){
+    clickCard.classList.add("flip");
+    if(!cardOne){
+
+        return cardOne = clickCard
+}
+cardTwo = clickCard;
+disableDeck = true;
+let cardOneImg = cardOne.querySelector("img").src,
+cardTwoImg = cardTwo.querySelector("img").src;
+matchCards(cardOneImg, cardTwoImg);
+    }
+}
+
+function matchCards(Img1,Img2){
+if(img1=== img2) {
+    matchedCard++;
+
+    if(matchedCard == 8){
+        shffleCard()
+    }
+ cardOne.removeEventListener("click", flipCard);
+ cardTwo.removeEventListener("click", flipCard);
+ cardOne = cardTwo = "";
+ return disableDeck = false;;
+}
+
+setTimeout(()=> {
+
+cardOne.classList,add("shake");
+cardTwo.classList.add("shake");
+}, 400);
+
+setTimeout(()=> {
+    
+    cardOne.classList,remove("shake","flip");
+    cardTwo.classList.remove("shake","flip");
+    cardOne = cardTwo = "";
+    disableDeck = false;
+    }, 1200);
+    }
+
+function shuffleCard() {
+     matchedCard = 0;
+     cardOne = cardTwo = "";
+     card.forEach(card =>{
+        card.addEventListener("click", flipCard);
+    });
+}
+
+card.forEach(card =>{
+    card.addEventListener("click", flipCard);
+});
